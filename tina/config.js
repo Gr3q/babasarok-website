d18d2b3b9794c6cc3bdbd232c4f446aed4cf807b
Attila Greguss <floyd0122@gmail.com>
2025-05-26 10:30:11 +0100

    fix default branch

--- tina/config.js
+++ tina/config.js
@@ -15,10 +15,9 @@ import {
 } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
+  branch: "main", // The branch you want to edit content on
   clientId: process.env.TINA_CMS_CLIENT_ID, // Get this from tina.io
   token: process.env.TINA_CMS_CLIENT_TOKEN, // Get this from tina.io
   client: { skip: true },
